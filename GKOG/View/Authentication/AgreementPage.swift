46864a58d2a75ff947d02df4d88f5d1d67157bb0
sujileelea <sujileelea@gmail.com>
2023-12-04 16:29:06 +0900

    hotfix: 프로젝트 이슈 해결 및 공통 적용 변수 작성

---
+++ GKOG/View/Authentication/AgreementPage.swift
@@ -0,0 +1,18 @@
+//
+//  AgreementPage.swift
+//  GKOG
+//
+//  Created by Suji Lee on 12/4/23.
+//
+
+import SwiftUI
+
+struct AgreementPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AgreementPage()
+}
---
+++ GKOG/View/Authentication/RegisterPage.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterPage.swift
+//  GKOG
+//
+//  Created by Suji Lee on 12/4/23.
+//
+
+import SwiftUI
+
+struct RegisterPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterPage()
+}
