5bb0ff30477a755179f3f6cfc0cf08d9a76f2915
sujileelea <sujileelea@gmail.com>
2023-12-05 19:32:10 +0900

    hotfix: Evnet 모델 수정

--- GKOG/Model/Event.swift
+++ GKOG/Model/Event.swift
@@ -8,13 +8,15 @@
 import Foundation
 
 struct Event: Codable, Hashable, Identifiable {
-    var id: Int?
+    var id: Int? // 카카오mapId
+    var hasJoined: Bool?
     var title: String?
     var detail: String?
     var startInfo: String? // "YY-mm-dd-hh-ss"
-    var endInfo: String?// "YY-mm-dd-hh-ss"
-    var lon: CGFloat?
-    var lat: CGFloat?
+    var endInfo: String? // "YY-mm-dd-hh-ss"
+    var roadAddress: String?
+    var longitude: CGFloat? //경도 (x)
+    var latitude: CGFloat? // 위도 (y)
     var category: String?
     var participants: [Member]?// 멤버 객체 리스트 List<Member>
     var comments : [Comment]? // 코멘트 객체 리스트 List<Comment>
